1d202d2150fd182e2f206adfc1b2486b6fb5fae0
minnje97 <dlalswp112@naver.com>
2021-08-28 19:05:59 +0900

    210827

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+app.get("/", logger, handleHome);
+
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
 
