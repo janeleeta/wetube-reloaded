3653bb0413f6408b4d83c6797f98dbae5dbe391d
minnje97 <dlalswp112@naver.com>
2021-09-02 19:10:51 +0900

    210901

---
+++ src/routers/globalRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { trending } from "../controllers/videoControllers";
+import { join } from "../controllers/userControllers";
+
+const globalRouter = express.Router();
+
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
+
+export default globalRouter;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-app.get("/", logger, handleHome);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
