5de31c80c06648d3adda37449cbb18e31e72ca9f
minnje97 <dlalswp112@naver.com>
2021-09-03 17:24:19 +0900

    210903-1

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { trending } from "../controllers/videoControllers";
-import { join } from "../controllers/userControllers";
+import { trending, search } from "../controllers/videoControllers";
+import { join, login } from "../controllers/userControllers";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
