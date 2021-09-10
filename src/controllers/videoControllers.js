0b4ac477af3867cd370966ebe95daf90142ae52c
minnje97 <dlalswp112@naver.com>
2021-09-10 22:51:37 +0900

    210907

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,8 +1,7 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", potato: "tomato" });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  return res.send("Delete Video");
-};
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("Delete Video");
