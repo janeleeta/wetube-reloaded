8707524350112e95457b70e5449164fef0c15dfb
minnje97 <dlalswp112@naver.com>
2021-09-13 21:10:07 +0900

    210912

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,8 +1,37 @@
+let videos = [
+  {
+    title: "First video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  res.render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
