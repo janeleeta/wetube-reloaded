be069fe455ddf699746ccb02bc8307a5852f02be
minnje97 <dlalswp112@naver.com>
2021-11-24 02:17:17 +0900

    211122

--- src/middlewares.js
+++ src/middlewares.js
@@ -26,7 +26,7 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limits: {
-    fileSize: 300,
+    fileSize: 3000000,
   },
 });
 
